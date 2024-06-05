9486acbe49e3ade4bc0aa42e80cb5875d0e339e3
sasa793 <122746103+sasa793@users.noreply.github.com>
2024-06-05 15:45:33 -0400

    Update get_the_books.sh

--- get_the_books.sh
+++ get_the_books.sh
@@ -10,5 +10,5 @@ book_ids=("932" "17192" "1064" "1063" "2148" "2147" "2150" "50852" "25266" "3203
 
 #loop through the book IDs and download each
 for book_id in "${book_ids[@]}"; do
-	wget -O "Books/pg${book_id}.txt" "https://www.gutenberg.org/ebooks/${book_id}.txt.utf-8"
+	wget -O "./pg${book_id}.txt" "https://www.gutenberg.org/ebooks/${book_id}.txt.utf-8"
 done
