4a85c7260b2f8385d159c3751bab6d9aa7d12a8c
Ubuntu <ubuntu@ip-172-31-21-36.ec2.internal>
2024-08-09 21:56:45 +0000

    Adjusted files to achieve a 10/10 pylint score

--- src/pkg_sme5qyx/tokenizer_functions.py
+++ src/pkg_sme5qyx/tokenizer_functions.py
@@ -1,9 +1,14 @@
-import logging
+"""
+This module contains functions for text processing including cleaning text,
+tokenizing text into words, and counting word frequencies.
+"""
+
+# import logging
 import re
 from collections import Counter
 
 # Configuration logging
-logging.basicConfig(level=logging.DEBUG, format='%(levelname)s:%(message)s')
+#logging.basicConfig(level=logging.DEBUG, format='%(levelname)s:%(message)s')
 
 def clean_text(input_text):
     """
@@ -25,9 +30,9 @@ def clean_text(input_text):
         If the input is not a string or if the output is not a string.
     """
     assert isinstance(input_text, str), "Input must be a string"
-    logging.debug("Cleaning text: %s", input_text)
+    # logging.debug("Cleaning text: %s", input_text)
     cleaned_text = re.sub(r'[^\w\s]', '', input_text.lower())
-    logging.debug("Cleaned text: %s", cleaned_text)
+    # logging.debug("Cleaned text: %s", cleaned_text)
     assert isinstance(cleaned_text, str), "Output must be a string"
     return cleaned_text
 
@@ -51,9 +56,9 @@ def tokenize(input_text):
         If the input is not a string or if the output is not a list.
     """
     assert isinstance(input_text, str), "Input must be a string"
-    logging.debug("Tokenizing text: %s", input_text)
+    # logging.debug("Tokenizing text: %s", input_text)
     tokens = clean_text(input_text).split()
-    logging.debug("Tokens: %s", tokens)
+    # logging.debug("Tokens: %s", tokens)
     assert isinstance(tokens, list), "Output must be a list"
     return tokens
 
@@ -77,9 +82,9 @@ def count_words(input_text):
         If the input is not a string or if the output is not a dictionary.
     """
     assert isinstance(input_text, str), "Input must be a string"
-    logging.debug("Counting words in text: %s", input_text)
+    # logging.debug("Counting words in text: %s", input_text)
     words = tokenize(input_text)
     word_counts = Counter(words)
-    logging.debug("Word counts: %s", word_counts)
+    # logging.debug("Word counts: %s", word_counts)
     assert isinstance(word_counts, dict), "Output must be a dictionary"
     return word_counts
